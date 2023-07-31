10dee7567af13417442a8f1cbe12c7c05e783b20
Kilian Lieret <kilian.lieret@posteo.de>
2023-07-31 15:19:05 -0400

    🚸 Implement timeout for wandb sync (#57)

--- tests/test_syncer.py
+++ tests/test_syncer.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 
 import logging
+import subprocess
+import unittest
+import unittest.mock
 from pathlib import Path
 
 from wandb_osh.syncer import WandbSyncer
@@ -20,3 +23,17 @@ def test_wandb_syncer(tmp_path, caplog):
     with caplog.at_level(logging.DEBUG):
         ws.loop()
     assert f"Command would be: wandb sync . in {target.resolve()}" in caplog.text
+
+
+def test_wandb_sync_timeout(tmp_path, caplog):
+    with unittest.mock.patch(
+        "wandb_osh.syncer.sync_dir", side_effect=subprocess.TimeoutExpired("asdf", 123)
+    ):
+        tmp_path = Path(tmp_path)
+        ws = WandbSyncer(tmp_path)
+        target = tmp_path / "test" / "123"
+        (tmp_path / "123.command").write_text(str(target.resolve()))
+        target.mkdir(parents=True)
+        with caplog.at_level(logging.DEBUG):
+            ws.loop()
+        assert "timed out. Trying later." in caplog.text
