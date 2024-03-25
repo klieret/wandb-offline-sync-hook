1bcd8bd6296f0d332d9b8ed6a3af958854dab15c
CedricLeon <leonard99cedric@gmail.com>
2024-03-25 14:50:15 +0100

    Updating the Ray Hook, with the new `TriggerWandbSyncHook` initialization and call

--- src/wandb_osh/ray_hooks.py
+++ src/wandb_osh/ray_hooks.py
@@ -12,15 +12,22 @@ _comm_default_dir = Path("~/.wandb_osh_command_dir").expanduser()
 
 
 class TriggerWandbSyncRayHook(LoggerCallback):
-    def __init__(self, communication_dir: PathLike = _comm_default_dir):
+    def __init__(
+        self,
+        communication_dir: PathLike = _comm_default_dir,
+        sync_every_n_epochs: int = 1,
+    ):
         """Hook to be used when interfacing wandb with ray tune.
 
         Args:
             communication_dir: Directory used for communication with wandb-osh.
+            sync_every_n_epochs: Number of epochs between each trigger (default = 1, every epoch).
         """
         super().__init__()
-        self._hook = TriggerWandbSyncHook(communication_dir=communication_dir)
+        self._hook = TriggerWandbSyncHook(
+            communication_dir=communication_dir, sync_every_n_epochs=sync_every_n_epochs
+        )
 
     def log_trial_result(self, iteration: int, trial: Trial, result: dict):
         trial_dir = Path(trial.logdir)
-        self._hook(trial_dir)
+        self._hook(trial_dir, current_epoch=iteration)
